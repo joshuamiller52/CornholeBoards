671066b7ab5fb6d189583a7b7f97839163342ab7
Joshua Miller <josde011@gmail.com>
2018-11-10 03:50:08 -0600

    Initial commit

---
+++ Scoreboard.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Scoreboard.Tests {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1 () {
+        }
+    }
+}
---
+++ Scoreboard/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ws281x.Net;
+namespace Scoreboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Neopixel(1, 18);
+            a.Begin();
+        }
+    }
+}
